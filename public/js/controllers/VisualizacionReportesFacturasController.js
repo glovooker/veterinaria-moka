7dad850cd1dbf752ccc722eb919c08ccfad2a71d
lvroc <lvroc.gnz@outlook.com>
2022-08-19 00:16:46 -0600

    en proceso
    Hice modificaciones al html , y elimine el codigo anterior que era de la primera iteracion y no me servia mucho para esta  iteracion.

--- public/js/controllers/VisualizacionReportesFacturasController.js
+++ public/js/controllers/VisualizacionReportesFacturasController.js
@@ -1,37 +1,72 @@
-let fecha = document.getElementById('dateFilter');
-var suma = 0;
+'use strict';
+/*para filtrar*/
+const inputFiltro = document.getElementById('txtFiltro');
+inputFiltro.addEventListener('keyup', ImprimirDatos);
 
-document.getElementById('bttn').addEventListener('submit',limpiar);
+let arregloListaFacturas = [];
+/****llamada de funcion***/
+ObtenerListaFacturas();
+/***********/
 
-function limpiar(){
-    formulario.reset();
+async function ObtenerListaFacturas() {
+    let result = await ObtenerFacturaBaseDatos();
+    if (result != {} && result.resultado == true) {
+        arregloListaFacturas = result.ListaFacturasDB;
+        ImprimirDatos();
+        console.log(arregloListaFacturas);
+    } else {
+        ImprimirMsjError(result.msj);
+        return;
+    }
 }
 
-const tablaReportes = document.querySelector('#tablaReportes tbody');
+async function ImprimirDatos() {
+    let tbody = document.getElementById('tablaReportes');
+    tbody.innerHTML = '';
+    let sumaTotal = 0;
+    let inputTotal = document.getElementById('txtTotal');
 
-imprimirCitas();
-imprimirResumenDinero();
-filtrarNum();
 
-function imprimirCitas(){ 
-    tablaReportes.innerHTML = '';
-    let listaCitas = obtenerFinanc();
-    for (let i = 0; i < listaCitas.length; i++) {
-        let fila = tablaReportes.insertRow();
-        fila.insertCell().innerHTML = listaCitas[i].factura; 
-        fila.insertCell().innerHTML = listaCitas[i].Dinero;
-        fila.insertCell().innerHTML = listaCitas[i].Fechas;
+    let filtro = inputFiltro.value;
+
+    for (let i = 0; i < arregloListaFacturas.length; i++) {
+        if(arregloListaFacturas[i].TotalAPagar.toString().includes(filtro)) {
+            let fila = tbody.insertRow();
+            let celdaNumeroFactura = fila.insertCell();
+            let celdaCliente = fila.insertCell();
+            let celdaFecha = fila.insertCell();
+            let celdaMontoFacturado = fila.insertCell();
+            let celdaAcciones = fila.insertCell();
+
+            let btnVer = document.createElement('button');
+            btnVer.onclick = function(){
+                location.href = 'VistaFactura.html?_id=' + arregloListaFacturas[i]._id + '&NumeroFactura=' + arregloListaFacturas.NumeroFactura;
+            };
+            btnVer.type = 'button';
+            btnVer.innerText = '🔍';
+            btnVer.title = 'VER FACTURA';
+            /* btnEdit.classList.add('btnsTabla'); */
+
+            
+           
+
+            let divBtns = document.createElement('div');
+            divBtns.appendChild(btnVer);
+            
+
+            celdaNumeroFactura.innerHTML = arregloListaFacturas[i].Estado;
+            celdaCliente.innerHTML = 'CLIENTE'
+
+            let fechaFacturacion = new Date(arregloListaFacturas[i].Fecha.replace('Z',''));
+            celdaFecha.innerHTML = fechaFacturacion.getDate() + '/' + (fechaFacturacion.getMonth() +1) + '/' + fechaFacturacion.getFullYear();
+            
+            celdaMontoFacturado.innerHTML = '₡'+arregloListaFacturas[i].TotalAPagar;
+            celdaAcciones.appendChild(divBtns);
+            sumaTotal= sumaTotal + arregloListaFacturas[i].TotalAPagar
+        }
+
+
     }
- };
- 
+    inputTotal.innerHTML = '₡'+ sumaTotal
 
-function imprimirResumenDinero(){
-    const resumenDinero = document.getElementById('resumenDineroTot') 
-    let dineroCantidad = obtenerDineroTotal();
-    let suma = 0;
-    for (let i = 0; i < dineroCantidad.length; i++) {
-        suma += dineroCantidad[i];
-    }
-    document.getElementById('resumenDinero').value = suma
 }
-
