8a9026e5c13901d3c53a64a5d428c56340cf8d50
lvroc <lvroc.gnz@outlook.com>
2022-08-20 17:40:24 -0600

    working on it
    modifique el html un poco y el controller original de la primera iteracion.

--- public/js/controllers/VisualizacionReportesCitasController.js
+++ public/js/controllers/VisualizacionReportesCitasController.js
@@ -1,35 +1,17 @@
 'use strict';
 
-let veterinario = document.getElementById('txtVet');
-let ranking = document.getElementById('ranking');
-let cliente = document.getElementById('txtUser');
-let mascota = document.getElementById('mascotatxt');
 
-const tablaReportes = document.querySelector('#tablaReportes tbody');
+let listaCitas = [];
 
-let mostrar = true;
+GetListaCitas();
 
-imprimirCitas();
-
-document.getElementById('bttn').addEventListener('submit',limpiar);
-document.getElementById('buscar').addEventListener('onclick', filtrar);
-
-function limpiar(){
-    formulario.reset();
-}
-
-
-function imprimirCitas(){ 
-    
-    tablaReportes.innerHTML = '';
-    let listaCitas = ObtenerCitas();
-    for (let i = 0; i < listaCitas.length; i++) {
-        let fila = tablaReportes.insertRow();
-        fila.insertCell().innerHTML = listaCitas[i].Cliente; 
-        fila.insertCell().innerHTML = listaCitas[i].Mascota;
-        fila.insertCell().innerHTML = listaCitas[i].Doctor;
-        fila.insertCell().innerHTML = listaCitas[i].Fechas;
-        fila.insertCell().innerHTML = listaCitas[i].Hora;
-        fila.insertCell().innerHTML = listaCitas[i].Ranking;
+async function GetListaCitas() {
+    let result = await ObtenerListaCitas('C');
+    if (result != {} && result.resultado == true) {
+        listaCitas = result.ListaCitasBD;       
+        ImprimirDatos(); 
+    } else {
+        imprimirMsjError(result.msj);
+        return;
     }
- }; 
+}
