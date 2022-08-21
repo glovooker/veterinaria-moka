849acb80e3d15b3a0d67112f5099d0aa6e777124
lvroc <lvroc.gnz@outlook.com>
2022-08-20 20:49:41 -0600

    update

--- public/js/controllers/VisualizacionReportesCitasController.js
+++ public/js/controllers/VisualizacionReportesCitasController.js
@@ -4,6 +4,8 @@ inputFiltro.addEventListener('keyup', ImprimirDatos);
 
 
 let listaCitas = [];
+let listaPersonas=[];
+let listaMascotas=[];
 /****llamada de funcion***/
 GetListaCitas()
 /******************/
@@ -11,7 +13,9 @@ GetListaCitas()
 async function GetListaCitas() {
     let result = await ObtenerListaCitas('C');
     if (result != {} && result.resultado == true) {
-        listaCitas = result.ListaCitasBD;    
+        listaCitas = result.ListaCitasBD;
+        await GetListaPersonas();  
+        await GetListaMascota(); 
         ImprimirDatos(); 
     } else {
         imprimirMsjError(result.msj);
@@ -20,26 +24,47 @@ async function GetListaCitas() {
 }
 
 
+async function GetListaPersonas() {
+    let result = await ObtenerListaPersonas();
+    if (result != {} && result.resultado == true) {
+        listaPersonas = result.ListaPersonasBD;
+    } else {
+        imprimirMsjError(result.msj);
+        return;
+    }
+}
+
+async function  GetListaMascota() {
+    let result = await ObtenerListaMascota();
+    if (result != {} && result.resultado == true) {
+        listaMascotas = result.ListaMascotaBD;
+    } else {
+        imprimirMsjError(result.msj);
+        return;
+    }
+}
 
 async function ImprimirDatos() {
     let filtro = inputFiltro.value;
     let tbody = document.getElementById('tablaReportes');
+    let vetNombre,clienteNombre,mascotaNombre; 
     tbody.innerHTML = '';
 
     
     
     for (let i = 0; i < listaCitas.length; i++) {
           
-        if(listaCitas[i].Estado.toLowerCase().includes(filtro)) {
+        if(ObtenerEstadoCita(listaCitas[i].Estado).toLowerCase().includes(filtro)) {
             let fila = tbody.insertRow();
+            let celdaFecha = fila.insertCell();
             let celdaNombreCliente = fila.insertCell();
             let celdaNombreMascota= fila.insertCell();
-            let celdaFecha = fila.insertCell();
             let celdaHoraInicio = fila.insertCell();
             let celdaHoraFinal = fila.insertCell();
             let celdaNombreVet = fila.insertCell();
             let celdaEstado = fila.insertCell();
             let celdaAcciones = fila.insertCell();
+            
 
             /* let btnVer = document.createElement('button');
             btnVer.onclick = function(){
@@ -50,52 +75,38 @@ async function ImprimirDatos() {
             btnVer.title = 'VER FACTURA';
             let divBtns = document.createElement('div');
             divBtns.appendChild(btnVer); */
-
-            celdaNombreCliente.innerHTML = cliente.Nombre;
-            celdaNombreMascota.innerHTML = mascota.Nombre;
-            celdaHoraInicio.innerHTML = listaCitas[i].HoraInicio;
-            celdaHoraFinal.innerHTML = listaCitas[i].HoraFinal;
             let fechaCita = new Date(listaCitas[i].FecInicio.replace('Z',''));
             celdaFecha.innerHTML = fechaCita.getDate() + '/' + (fechaCita.getMonth() +1) + '/' + fechaCita.getFullYear();
-            celdaNombreVet.innerHTML = veterinario.Nombre;
+
+            for (let j = 0; j < listaPersonas.length; j++){
+                if(listaPersonas[j]._id===listaCitas[i]._idCliente){
+                    clienteNombre = listaPersonas[j].Nombre;
+                    break;
+                }
+            }
+            celdaNombreCliente.innerHTML = clienteNombre;
+            for (let k = 0; k < listaMascotas.length; k++){
+                if(listaMascotas[k]._id===listaCitas[i]._idMascota){
+                    mascotaNombre = listaMascotas[k].Nombre;
+                    break;
+                }
+            }
+            celdaNombreMascota.innerHTML = mascotaNombre;
+            
+            for (let l = 0; l < listaPersonas.length; l++){
+                if(listaPersonas[l]._id===listaCitas[i]._idVeterinario){
+                    vetNombre = listaPersonas[l].Nombre;
+                    break;
+                }
+            }
+            
+
+            celdaHoraInicio.innerHTML = listaCitas[i].HoraInicio;
+            celdaHoraFinal.innerHTML = listaCitas[i].HoraFinal;
+            
+            celdaNombreVet.innerHTML = vetNombre;
             celdaEstado.innerHTML = ObtenerEstadoCita(listaCitas[i].Estado);
 
         }
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            
-           
-
-            
-            
-            
-    
-         
-
-            
-    
-
-
-
-
-    
-    
-
-    
-
-
