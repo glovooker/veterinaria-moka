b1a9107073ef00c62b71cf4b570f04532b3deb6a
Gabriel Lobo <globo.coding@gmail.com>
2022-08-22 00:12:39 -0600

    Fixed Local Storage Reference

--- public/js/controllers/EnlistarMetodosPagoController.js
+++ public/js/controllers/EnlistarMetodosPagoController.js
@@ -1,10 +1,10 @@
-"use strict";
+'use strict';
 
-const cuerpoTabla = document.querySelector("#tblTarjetas tbody");
+const cuerpoTabla = document.querySelector('#tblTarjetas tbody');
 let tarjetas = [];
 
 const inicializarListas = async () => {
-  let persona = JSON.parse(localStorage.getItem("datosPersonaConsultada"));
+  let persona = JSON.parse(localStorage.getItem('datosSesionActiva'));
   let _idC = persona._id;
   // console.log(_idC);
   tarjetas = await getDatos(`/ListarTarjetasCliente?_idC=${_idC}`);
@@ -14,38 +14,38 @@ const inicializarListas = async () => {
 };
 
 const mostrarTabla = async () => {
-  cuerpoTabla.innerHTML = "";
+  cuerpoTabla.innerHTML = '';
 
   tarjetas.forEach((tarjeta) => {
     let fila = cuerpoTabla.insertRow();
     let numerot = String(tarjeta.NumTarjeta);
     // fila.insertCell().innerText = tarjeta._idC;
-    fila.insertCell().innerText = "************" + numerot.slice(-4);
+    fila.insertCell().innerText = '************' + numerot.slice(-4);
     fila.insertCell().innerText = tarjeta.FecExpira;
     fila.insertCell().innerText = tarjeta.Nombre;
     let tdAcciones = fila.insertCell();
 
     //Creación del botón de eliminar
-    let btnEliminar = document.createElement("button");
-    btnEliminar.textContent = "Eliminar";
-    btnEliminar.type = "button";
-    btnEliminar.classList.add("btn-eliminar");
+    let btnEliminar = document.createElement('button');
+    btnEliminar.textContent = 'Eliminar';
+    btnEliminar.type = 'button';
+    btnEliminar.classList.add('btn-eliminar');
 
     //Agregar el botón de editar y eliminar a la celda de acciones
     tdAcciones.appendChild(btnEliminar);
-    btnEliminar.addEventListener("click", () => {
+    btnEliminar.addEventListener('click', () => {
       Swal.fire({
-        title: "¿Está seguro que desea eliminar la información?",
-        text: "La acción no se puede revertir",
-        icon: "warning",
+        title: '¿Está seguro que desea eliminar la información?',
+        text: 'La acción no se puede revertir',
+        icon: 'warning',
         showCancelButton: true,
-        confirmButtonColor: "#3085d6",
-        cancelButtonColor: "#d33",
-        confirmButtonText: "¡Sí, eliminar!",
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: '¡Sí, eliminar!',
       }).then((result) => {
         if (result.isConfirmed) {
-          eliminarDatos("/EliminarTarjetas", tarjeta._idC);
-          Swal.fire("¡Tarjeta eliminada!", "La tarjeta fue borrada", "success");
+          eliminarDatos('/EliminarTarjetas', tarjeta._idC);
+          Swal.fire('¡Tarjeta eliminada!', 'La tarjeta fue borrada', 'success');
         }
       });
     });
--- public/js/controllers/MetodosPagoController.js
+++ public/js/controllers/MetodosPagoController.js
@@ -1,6 +1,6 @@
-"use strict";
+'use strict';
 
-btnRegistrar.addEventListener("click", Validaciones);
+btnRegistrar.addEventListener('click', Validaciones);
 
 function Validaciones() {
   if (ValidarCampos() == false) {
@@ -12,34 +12,34 @@ function Validaciones() {
     CrearTarjeta();
     /***********************************************************************/
     Swal.fire({
-      title: "Success!",
-      text: "Todos los campos requeridos han sido ingresados",
-      icon: "success",
-      confirmButtonText: "Ok",
+      title: 'Success!',
+      text: 'Todos los campos requeridos han sido ingresados',
+      icon: 'success',
+      confirmButtonText: 'Ok',
     });
     return true;
   }
 }
 
 function ValidarFecha() {
-  let fecha = document.getElementById("txtExpiracion");
+  let fecha = document.getElementById('txtExpiracion');
   if (new Date(fecha.value) < new Date()) {
     //fecha debe ser mayor a HOY
     ImprimirMsjError(
-      "Fecha de Expiración debe ser superior a hoy, ¡Favor validar!"
+      'Fecha de Expiración debe ser superior a hoy, ¡Favor validar!'
     );
-    ResaltarInputInvalido("txtExpiracion");
-    ResaltarLabelInvalido("lblExpiracion");
+    ResaltarInputInvalido('txtExpiracion');
+    ResaltarLabelInvalido('lblExpiracion');
     return false;
   }
 }
 
 let CrearTarjeta = () => {
-  let numTarjeta = document.getElementById("txtNumTarjeta");
-  let expiracion = document.getElementById("txtExpiracion");
-  let ccv = document.getElementById("txtCVV");
-  let nombreTarjeta = document.getElementById("txtNombreTarjeta");
-  let persona = JSON.parse(localStorage.getItem("datosPersonaConsultada"));
+  let numTarjeta = document.getElementById('txtNumTarjeta');
+  let expiracion = document.getElementById('txtExpiracion');
+  let ccv = document.getElementById('txtCVV');
+  let nombreTarjeta = document.getElementById('txtNombreTarjeta');
+  let persona = JSON.parse(localStorage.getItem('datosSesionActiva'));
   // console.log(persona);
   let Tarjeta = {
     _idC: persona._id,
@@ -48,5 +48,5 @@ let CrearTarjeta = () => {
     Cvv: ccv.value,
     Nombre: nombreTarjeta.value,
   };
-  RegistrarTarjetaDatos("RegistrarTarjeta", Tarjeta);
+  RegistrarTarjetaDatos('RegistrarTarjeta', Tarjeta);
 };
