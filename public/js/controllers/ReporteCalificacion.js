e17e8df6693cf3733ec66377d9b697b899a79e99
lvroc <lvroc.gnz@outlook.com>
2022-08-22 21:05:55 -0600

    update

--- public/js/controllers/ReporteCalificacion.js
+++ public/js/controllers/ReporteCalificacion.js
@@ -175,10 +175,3 @@ async function ImprimirDatos(listaMascotas) {
 }
     
     
-function trunc (x, posiciones = 0) {
-    var s = x.toString()
-    var l = s.length
-    var decimalLength = s.indexOf('.') + 1
-    var numStr = s.substr(0, decimalLength + posiciones)
-    return Number(numStr)
-  }
