6e174ac63076b0c96b6aa89b3b7ecc19d29efcd2
Len <villalobosrojas22@gmail.com>
2022-08-26 18:01:57 -0600

    Update PerfilMascotaController.js

--- public/js/controllers/PerfilMascotaController.js
+++ public/js/controllers/PerfilMascotaController.js
@@ -15,7 +15,7 @@ async function desplegarDatosConsultados() {
     document.getElementById('outNombre').value = mascotaConsultada.Nombre;
     document.getElementById('txtTipoMascota').value = mascotaConsultada.Especie;
     document.getElementById('txtDueñoMascota').value = owner.Nombre;
-    document.getElementById('txtPadecimientos').value =
+    // document.getElementById('txtPadecimientos').value
     document.getElementById('MascotaFoto').src = mascotaConsultada.FotoMascota;
       mascotaConsultada.Observaciones;
   }
